b2490d261bcc0e040fa8d4bed347bb8e77344bc2
Natalia Rodriguez <32943728+nrodriguez99@users.noreply.github.com>
2019-08-26 23:39:30 -0600

    Validar no se repita nombre del proyecto y muestreo Reunion#4

--- AppProdu/AppProdu/CrearMuestreo.xaml.cs
+++ AppProdu/AppProdu/CrearMuestreo.xaml.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -18,6 +19,8 @@ namespace AppProdu
 	{
         Dictionary<int, string> samplingTypes = new Dictionary<int, string>();
         List<UserType> types;
+        ObservableCollection<Sampling> Items;
+        List<Sampling> muestreos;
 
         public CrearMuestreo ()
 		{
@@ -31,48 +34,55 @@ namespace AppProdu
         {
             if (!String.IsNullOrEmpty(nombreMuestreoEntry.Text) && !String.IsNullOrEmpty(cantMuestrasEntry.Text))
             {
-                var client = new HttpClient
+                if (await getSamplings(nombreMuestreoEntry.Text) == false)
                 {
-                    BaseAddress = new Uri("https://app-produ.herokuapp.com")
-                };
-                var idType = samplingTypes.FirstOrDefault(x => x.Value == tipo.SelectedItem.ToString()).Key;
-                var newSampling = new Sampling
-                {
-                    nombre = nombreMuestreoEntry.Text,
-                    cantMuestras = System.Convert.ToInt32(cantMuestrasEntry.Text),
-                    cantMuestrasTotal = System.Convert.ToInt32(cantMuestrasEntry.Text),
-                    descripcion = descripcionMuestreoEditor.Text,
-                    fase = 1,
-                    sampling_type_id = idType,
-                    project_id = (int)Application.Current.Properties["id-project"],
-                    muestrasActual = 0,
-                    token = Application.Current.Properties["currentToken"].ToString()
-                };
-                string jsonData = JsonConvert.SerializeObject(newSampling);
-                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-                HttpResponseMessage response = await client.PostAsync("/samplings/newsampling.json", content);
-                if (response.StatusCode == HttpStatusCode.Created)
-                {
-                    var result = await response.Content.ReadAsStringAsync();
-                    var jobject = JObject.Parse(result);
-                    var data = JsonConvert.DeserializeObject<Sampling>(jobject["muestreo"].ToString());
-
-
-                    try
+                    var client = new HttpClient
                     {
-                        Application.Current.Properties["id-sampling"] = data.id;
-                        Application.Current.Properties["fase"] = data.fase;
-                        Application.Current.Properties["sampling-type-id"] = data.sampling_type_id;
-                        await crearFaseAsync();
-                        var etapasPage = new Etapas();
-                        await Navigation.PushAsync(etapasPage);
-                        this.Navigation.RemovePage(this.Navigation.NavigationStack[this.Navigation.NavigationStack.Count - 2]);
-
-                    }
-                    catch (Exception)
+                        BaseAddress = new Uri("https://app-produ.herokuapp.com")
+                    };
+                    var idType = samplingTypes.FirstOrDefault(x => x.Value == tipo.SelectedItem.ToString()).Key;
+                    var newSampling = new Sampling
                     {
+                        nombre = nombreMuestreoEntry.Text,
+                        cantMuestras = System.Convert.ToInt32(cantMuestrasEntry.Text),
+                        cantMuestrasTotal = System.Convert.ToInt32(cantMuestrasEntry.Text),
+                        descripcion = descripcionMuestreoEditor.Text,
+                        fase = 1,
+                        sampling_type_id = idType,
+                        project_id = (int)Application.Current.Properties["id-project"],
+                        muestrasActual = 0,
+                        token = Application.Current.Properties["currentToken"].ToString()
+                    };
+                    string jsonData = JsonConvert.SerializeObject(newSampling);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync("/samplings/newsampling.json", content);
+                    if (response.StatusCode == HttpStatusCode.Created)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        var jobject = JObject.Parse(result);
+                        var data = JsonConvert.DeserializeObject<Sampling>(jobject["muestreo"].ToString());
+
+
+                        try
+                        {
+                            Application.Current.Properties["id-sampling"] = data.id;
+                            Application.Current.Properties["fase"] = data.fase;
+                            Application.Current.Properties["sampling-type-id"] = data.sampling_type_id;
+                            await crearFaseAsync();
+                            var etapasPage = new Etapas();
+                            await Navigation.PushAsync(etapasPage);
+                            this.Navigation.RemovePage(this.Navigation.NavigationStack[this.Navigation.NavigationStack.Count - 2]);
+
+                        }
+                        catch (Exception)
+                        {
+                        }
                     }
+                }
+                else
+                {
+                    await DisplayAlert("Error!", "El nombre del muestreo ya existe.", "OK");
                 }
             }
             else
@@ -147,6 +157,47 @@ namespace AppProdu
         private void Tipo_SelectedIndexChanged(object sender, EventArgs e)
         {
             var idType = samplingTypes.FirstOrDefault(x => x.Value == tipo.SelectedItem.ToString()).Key;
+        }
+
+
+        public async Task<bool> getSamplings(string nombreMuestreo)
+        {
+            try
+            {
+                var client = new HttpClient
+                {
+                    BaseAddress = new Uri("https://app-produ.herokuapp.com")
+                };
+                var userLogged = new User
+                {
+                    id = (int)Application.Current.Properties["id-project"],
+                    token = Application.Current.Properties["currentToken"].ToString()
+                };
+                string jsonData = JsonConvert.SerializeObject(userLogged);
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("/samplings/projectsamplings.json", content);
+
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var jobject = JObject.Parse(responseBody);
+                muestreos = JsonConvert.DeserializeObject<List<Sampling>>(jobject["samplings"].ToString());
+
+                Items = new ObservableCollection<Sampling> { };
+                for (int i = 0; i < muestreos.Count; i++)
+                {
+                    Sampling pro = muestreos[i];
+                    Items.Add(pro);
+                    Console.WriteLine(pro.nombre);
+                    if (pro.nombre == nombreMuestreo)
+                        return true;
+                }
+                return false;
+            }
+            catch (HttpRequestException e)
+            {
+            }
+            return false;
         }
     }
 }
--- AppProdu/AppProdu/CrearProyecto.xaml.cs
+++ AppProdu/AppProdu/CrearProyecto.xaml.cs
@@ -19,6 +19,8 @@ namespace AppProdu
 	{
 
         public ObservableCollection<string> Items { get; set; }
+        ObservableCollection<Project> ItemsProyectos = new ObservableCollection<Project> { };
+        List<Project> proyectos;
 
         public CrearProyecto ()
 		{
@@ -41,50 +43,95 @@ namespace AppProdu
         private async Task crearPro_Clicked(object sender, EventArgs e)
         {
             if (!String.IsNullOrEmpty(nombreProyectoEntry.Text)) {
-                var client = new HttpClient
-                {
-                    BaseAddress = new Uri("https://app-produ.herokuapp.com")
-                };
-                var newProject = new Project
-                {
-                    nombre = nombreProyectoEntry.Text,
-                    descripcion = descripcionProyectoEditor.Text,
-                    user_id = (int)Application.Current.Properties["id"],
-                    token = Application.Current.Properties["currentToken"].ToString()
-                };
-                string jsonData = JsonConvert.SerializeObject(newProject);
-                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-                HttpResponseMessage response = await client.PostAsync("/projects/newproject.json", content);
-                if (response.StatusCode == HttpStatusCode.Created)
-                {
-                    var result = await response.Content.ReadAsStringAsync();
-                    var jobject = JObject.Parse(result);
-                    var data = JsonConvert.DeserializeObject<Project>(jobject["proyecto"].ToString());
-
-
-                    try
+                if (await getProjects(nombreProyectoEntry.Text) == false) {
+                    var client = new HttpClient
                     {
-                        Application.Current.Properties["id-project"] = data.id;
-                        var muestreosPage = new Muestreos();
-                        await Navigation.PushAsync(muestreosPage);
-
-                        this.Navigation.RemovePage(this.Navigation.NavigationStack[this.Navigation.NavigationStack.Count - 2]);
-
-
-                    }
-                    catch (Exception)
+                        BaseAddress = new Uri("https://app-produ.herokuapp.com")
+                    };
+                    var newProject = new Project
                     {
+                        nombre = nombreProyectoEntry.Text,
+                        descripcion = descripcionProyectoEditor.Text,
+                        user_id = (int)Application.Current.Properties["id"],
+                        token = Application.Current.Properties["currentToken"].ToString()
+                    };
+                    string jsonData = JsonConvert.SerializeObject(newProject);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync("/projects/newproject.json", content);
+                    if (response.StatusCode == HttpStatusCode.Created)
+                    {
+                        var result = await response.Content.ReadAsStringAsync();
+                        var jobject = JObject.Parse(result);
+                        var data = JsonConvert.DeserializeObject<Project>(jobject["proyecto"].ToString());
+
+
+                        try
+                        {
+                            Application.Current.Properties["id-project"] = data.id;
+                            var muestreosPage = new Muestreos();
+                            await Navigation.PushAsync(muestreosPage);
+
+                            this.Navigation.RemovePage(this.Navigation.NavigationStack[this.Navigation.NavigationStack.Count - 2]);
+                        }
+                        catch (Exception)
+                        {
+                        }
                     }
                 }
                 else
                 {
+                    await DisplayAlert("Error!", "El nombre del proyecto ya existe.", "OK");
                 }
             }
             else {
                 await DisplayAlert("Error!", "Espacios vacíos!\nPor favor inserte el Nombre del Proyecto!", "OK");
             }
         }
+
+
+        public async Task<bool> getProjects(string nombreProyecto)
+        {
+            try
+            {
+                var client = new HttpClient
+                {
+                    BaseAddress = new Uri("https://app-produ.herokuapp.com")
+                };
+                var userLogged = new User
+                {
+                    id = (int)Application.Current.Properties["id"],
+                    token = Application.Current.Properties["currentToken"].ToString()
+                };
+                string jsonData = JsonConvert.SerializeObject(userLogged);
+                var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync("/projects/userprojects.json", content);
+
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var jobject = JObject.Parse(responseBody);
+                proyectos = JsonConvert.DeserializeObject<List<Project>>(jobject["projects"].ToString());
+
+
+                ItemsProyectos = new ObservableCollection<Project>();
+                for (int i = 0; i < proyectos.Count; i++)
+                {
+                    Project pro = proyectos[i];
+                    ItemsProyectos.Add(pro);
+                    Console.WriteLine(pro.nombre);
+                    if (pro.nombre == nombreProyecto)
+                        return true;
+                }
+                return false;
+
+            }
+            catch (HttpRequestException e)
+            {
+            }
+            return false;
+        }
+
         /*
         private async Task agregarColab_Clicked()
         {
